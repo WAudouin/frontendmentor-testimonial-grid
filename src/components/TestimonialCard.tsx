51f820213b7625fa79690d04f9a0d052df2e84c8
WAudouin <waudouin@amiltone.com>
2025-04-24 17:27:31 +0200

    feat: add quotation on purple card + fix font family

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,3 +1,4 @@
+import QuotationIcon from "../assets/bg-pattern-quotation.svg";
 import { Testimonial, variantStyles } from "../data";
 
 type TestimonialCardProps = {
@@ -8,12 +9,20 @@ const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
   const styles = variantStyles[testimonial.variant];
   return (
     <article
-      className={`p-8 ${testimonial.grid_classes} text-left ${styles.background} flex flex-col gap-4 rounded-lg shadow-2xl/40`}
+      className={`p-8 ${testimonial.grid_classes} text-left ${styles.background} relative flex flex-col gap-4 rounded-lg shadow-2xl/40`}
     >
       <section className="flex gap-4 items-center">
+        {testimonial.variant === "purple" && (
+          <img
+            alt="Quotation"
+            className="absolute right-15 top-1"
+            src={QuotationIcon}
+          />
+        )}
         <img
           className="rounded-4xl object-cover w-8 h-8"
           src={testimonial.user.profile_picture}
+          alt={`${testimonial.user.name}`}
         />
         <section className={`flex flex-col gap-1`}>
           <p
@@ -34,10 +43,10 @@ const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
           </p>
         </section>
       </section>
-      <p className={`font-semibold text-xl ${styles.title}`}>
+      <p className={`font-semibold text-xl z-10 ${styles.title}`}>
         {testimonial.title}
       </p>
-      <span className={`text-sm ${styles.text}`}>
+      <span className={`text-[13px] ${styles.text}`}>
         {"“ "}
         {testimonial.content}
         {" “"}
